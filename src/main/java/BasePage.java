308a63070ca01ca535eb5978ee26e367f6542d9e
Mykhailo Bidnyi <mbi@compent.net>
2021-03-05 17:38:01 +0200

    Add logic class

--- src/main/java/BasePage.java
+++ src/main/java/BasePage.java
@@ -1,2 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import utils.Links;
+import utils.Waiters;
+
+import java.util.concurrent.TimeUnit;
+
 public class BasePage {
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void openPage(String pageUrl) {
+        driver.get(Links.STAGE_BASIC_URL + pageUrl);
+        Waiters.implicitWait(driver, Waiters.TIME_TEN, TimeUnit.SECONDS);
+    }
 }
---
+++ src/main/java/utils/Links.java
@@ -0,0 +1,5 @@
+package utils;
+
+public class Links {
+    public static final String STAGE_BASIC_URL = "https://www.staging.board-office.dk/";
+}
