9e5b79362de168ccb6b00a8b4da745634692d63a
Mykhailo Bidnyi <mbi@compent.net>
2021-03-26 21:39:43 +0200

    Add locators

--- src/main/java/BoardGovernace/loginPage/Registration.java
+++ src/main/java/BoardGovernace/loginPage/Registration.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class Registration extends BasePage {
     public Registration(WebDriver driver) { super(driver); }
     @FindBy(name = "firstname")
@@ -20,18 +22,22 @@ public class Registration extends BasePage {
     WebElement passwordField;
     @FindBy(className = "error-listerror-list")
     WebElement errorBlock;
+    @FindBy(className = "error-list__item")
+    List<WebElement> validationList;
+    @FindBy(xpath = "/html/body/div[3]/div[1]/signup-form/div[2]/div/form/div/div/div[2]/div[2]/div/div/label/input")
+    WebElement conditionsCheckBox;
+    @FindBy(className = "subscription-label")
+    List<WebElement> tariffPlan;
+    @FindBy(xpath = "/html/body/div[3]/div[1]/signup-form/div[2]/div/form/div/div/div[2]/div[2]/div/button")
+    WebElement createProfileButton;
 
 
 
     public void fillFields(String firstName, String middleName, String surName, String email, String password) {
         firstNameField.sendKeys(firstName);
-        Waiters.treadWaiter(1);
         middleNameField.sendKeys(middleName);
-        Waiters.treadWaiter(1);
         surnameField.sendKeys(surName);
-        Waiters.treadWaiter(1);
         emailField.sendKeys(email);
-        Waiters.treadWaiter(1);
         passwordField.sendKeys(password);
         Waiters.treadWaiter(10);
     }
