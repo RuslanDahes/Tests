86ad925247d7ce8caa91b01c33543557c5228b15
Mykhailo Bidnyi <mbi@compent.net>
2021-12-13 15:10:52 +0200

    Update path

--- src/main/java/BoardGovernace/utils/AttachPath.java
+++ src/main/java/BoardGovernace/utils/AttachPath.java
@@ -1,26 +1,26 @@
 package BoardGovernace.utils;
 
 public class AttachPath {
-    public static String TEST_UPLOAD_PDF = "/src/test/resources/pdf-test.pdf";
-    public static String TEST_UPLOAD_DOC = "/src/test/resources/TestDOC.doc";
-    public static String TEST_UPLOAD_DOCX = "/src/test/resources/TestDOCX.docx";
-    public static String TEST_UPLOAD_TXT = "/src/test/resources/txtfile.txt";
-    public static String TEST_UPLOAD_XLS = "/src/test/resources/XLSfile.xls";
-
-    public static String TEST_UPLOAD_GIF = "/src/test/resources/gifpicture.gif";
-    public static String TEST_UPLOAD_JPG = "/src/test/resources/image.jpg";
-    public static String TEST_UPLOAD_JPEG = "/src/test/resources/photo.jpeg";
-    public static String TEST_UPLOAD_PNG = "/src/test/resources/testpicture.png";
-    public static String FILE_FOR_MOVE = "/src/test/resources/AttachmentFile/fileformove";
-
-//    public static String TEST_UPLOAD_PDF = "src/test/resources/pdf-test.pdf";
-//    public static String TEST_UPLOAD_DOC = "src/test/resources/TestDOC.doc";
-//    public static String TEST_UPLOAD_DOCX = "src/test/resources/TestDOCX.docx";
-//    public static String TEST_UPLOAD_TXT = "src/test/resources/txtfile.txt";
-//    public static String TEST_UPLOAD_XLS = "src/test/resources/XLSfile.xls";
+//    public static String TEST_UPLOAD_PDF = "/src/test/resources/pdf-test.pdf";
+//    public static String TEST_UPLOAD_DOC = "/src/test/resources/TestDOC.doc";
+//    public static String TEST_UPLOAD_DOCX = "/src/test/resources/TestDOCX.docx";
+//    public static String TEST_UPLOAD_TXT = "/src/test/resources/txtfile.txt";
+//    public static String TEST_UPLOAD_XLS = "/src/test/resources/XLSfile.xls";
 //
-//    public static String TEST_UPLOAD_GIF = "src/test/resources/gifpicture.gif";
-//    public static String TEST_UPLOAD_JPG = "src/test/resources/image.jpg";
-//    public static String TEST_UPLOAD_JPEG = "src/test/resources/photo.jpeg";
-//    public static String TEST_UPLOAD_PNG = "src/test/resources/testpicture.png";
+//    public static String TEST_UPLOAD_GIF = "/src/test/resources/gifpicture.gif";
+//    public static String TEST_UPLOAD_JPG = "/src/test/resources/image.jpg";
+//    public static String TEST_UPLOAD_JPEG = "/src/test/resources/photo.jpeg";
+//    public static String TEST_UPLOAD_PNG = "/src/test/resources/testpicture.png";
+//    public static String FILE_FOR_MOVE = "/src/test/resources/AttachmentFile/fileformove";
+
+    public static String TEST_UPLOAD_PDF = "src/test/resources/pdf-test.pdf";
+    public static String TEST_UPLOAD_DOC = "src/test/resources/TestDOC.doc";
+    public static String TEST_UPLOAD_DOCX = "src/test/resources/TestDOCX.docx";
+    public static String TEST_UPLOAD_TXT = "src/test/resources/txtfile.txt";
+    public static String TEST_UPLOAD_XLS = "src/test/resources/XLSfile.xls";
+
+    public static String TEST_UPLOAD_GIF = "src/test/resources/gifpicture.gif";
+    public static String TEST_UPLOAD_JPG = "src/test/resources/image.jpg";
+    public static String TEST_UPLOAD_JPEG = "src/test/resources/photo.jpeg";
+    public static String TEST_UPLOAD_PNG = "src/test/resources/testpicture.png";
 }
--- src/test/java/BoardGovernanceTest/BaseTest.java
+++ src/test/java/BoardGovernanceTest/BaseTest.java
@@ -30,8 +30,8 @@ public class BaseTest {
 
     @BeforeClass
     public void openBrowser() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
-//        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriverwin.exe");
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriverwin.exe");
         System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver");
         System.setProperty("webdriver.safari.driver", "src/test/resources/drivers/safaridriver");
 
