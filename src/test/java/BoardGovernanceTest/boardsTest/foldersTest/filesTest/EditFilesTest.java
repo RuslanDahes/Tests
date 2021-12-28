5be130d370401abae6aef74d347f68ec777be911
Mykhailo Bidnyi <mbi@compent.net>
2021-12-28 15:59:30 +0200

    Add testrail id and edit testsuites

--- src/test/java/BoardGovernanceTest/boardsTest/foldersTest/filesTest/EditFilesTest.java
+++ src/test/java/BoardGovernanceTest/boardsTest/foldersTest/filesTest/EditFilesTest.java
@@ -3,6 +3,9 @@ package BoardGovernanceTest.boardsTest.foldersTest.filesTest;
 import BoardGovernace.boards.folders.files.EditFiles;
 import BoardGovernace.boards.folders.files.UploadFiles;
 import BoardGovernace.loginPage.Login;
+import BoardGovernace.testrailConfig.APIException;
+import BoardGovernace.testrailConfig.TestRails;
+import BoardGovernace.testrailConfig.TestSuiteCreating;
 import BoardGovernace.utils.AttachPath;
 import BoardGovernace.utils.Texts;
 import BoardGovernace.utils.UserCredentials;
@@ -10,10 +13,17 @@ import BoardGovernace.utils.Waiters;
 import BoardGovernanceTest.BaseTest;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditFilesTest extends BaseTest  {
     @BeforeClass
@@ -23,7 +33,17 @@ public class EditFilesTest extends BaseTest  {
 //        editFiles.uploadFile(AttachPath.TEST_UPLOAD_DOC);
     }
 
+    @BeforeMethod
+    public void getTestCase(ITestContext ctx, Method method) throws NoSuchMethodException {
+        Method m = EditFilesTest.class.getMethod(method.getName());
+        if (m.isAnnotationPresent(TestRails.class)) {
+            TestRails ta = m.getAnnotation(TestRails.class);
+            System.out.println(ta.id());
+            ctx.setAttribute("caseId",ta.id());
+        }
+    }
 
+    @TestRails(id="87590")
     @Test
     public void editFileWithDescription() {
         EditFiles editFiles = new EditFiles(getDriver());
@@ -31,6 +51,7 @@ public class EditFilesTest extends BaseTest  {
 //        Assert.assertTrue(editFiles.checkFileInList("Edited File Name"));
     }
 
+    @TestRails(id="87591")
     @Test
     public void editFileWithOutDescription() {
         EditFiles editFiles = new EditFiles(getDriver());
@@ -44,5 +65,17 @@ public class EditFilesTest extends BaseTest  {
         editFiles.backToFolder();
     }
 
-
+    @AfterMethod
+    public void setTestCase(ITestResult result, ITestContext ctx) throws IOException, APIException {
+        Map data = new HashMap();
+        if(result.isSuccess()) {
+            data.put("status_id",1);
+        }
+        else {
+            data.put("status_id", 5);
+            data.put("comment", result.getThrowable().toString());
+        }
+        String caseId = (String)ctx.getAttribute("caseId");
+        TestSuiteCreating.CLIENT.sendPost("add_result_for_case/"+TestSuiteCreating.TEST_RUN+"/"+caseId,data);
+    }
 }
--- src/test/java/BoardGovernanceTest/boardsTest/foldersTest/filesTest/MoveFilesTest.java
+++ src/test/java/BoardGovernanceTest/boardsTest/foldersTest/filesTest/MoveFilesTest.java
@@ -3,13 +3,25 @@ package BoardGovernanceTest.boardsTest.foldersTest.filesTest;
 import BoardGovernace.boards.folders.files.MoveFiles;
 import BoardGovernace.boards.folders.files.UploadFiles;
 import BoardGovernace.loginPage.Login;
+import BoardGovernace.testrailConfig.APIException;
+import BoardGovernace.testrailConfig.TestRails;
+import BoardGovernace.testrailConfig.TestSuiteCreating;
 import BoardGovernace.utils.AttachPath;
 import BoardGovernace.utils.Texts;
 import BoardGovernace.utils.UserCredentials;
 import BoardGovernace.utils.Waiters;
 import BoardGovernanceTest.BaseTest;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MoveFilesTest extends BaseTest {
     @BeforeClass
@@ -18,12 +30,34 @@ public class MoveFilesTest extends BaseTest {
         moveFiles.openFolder();
     }
 
+    @BeforeMethod
+    public void getTestCase(ITestContext ctx, Method method) throws NoSuchMethodException {
+        Method m = MoveFilesTest.class.getMethod(method.getName());
+        if (m.isAnnotationPresent(TestRails.class)) {
+            TestRails ta = m.getAnnotation(TestRails.class);
+            System.out.println(ta.id());
+            ctx.setAttribute("caseId",ta.id());
+        }
+    }
+
+    @TestRails(id="87592")
     @Test
     public void moveFileS() {
         MoveFiles moveFiles = new MoveFiles(getDriver());
         moveFiles.moveFile();
-        Waiters.treadWaiter(10);
     }
 
-
+    @AfterMethod
+    public void setTestCase(ITestResult result, ITestContext ctx) throws IOException, APIException {
+        Map data = new HashMap();
+        if(result.isSuccess()) {
+            data.put("status_id",1);
+        }
+        else {
+            data.put("status_id", 5);
+            data.put("comment", result.getThrowable().toString());
+        }
+        String caseId = (String)ctx.getAttribute("caseId");
+        TestSuiteCreating.CLIENT.sendPost("add_result_for_case/"+TestSuiteCreating.TEST_RUN+"/"+caseId,data);
+    }
 }
